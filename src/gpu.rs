98734f5aa7e665beb1bf6ebb6e9ef9bec6463c97
Zimbobub <89832179+Zimbobub@users.noreply.github.com>
2026-01-21 01:01:00 +1100

    matrix multiplier basic abstraction, shader not impl

--- src/gpu.rs
+++ src/gpu.rs
@@ -2,6 +2,8 @@ use flume::bounded;
 use wgpu::util::{BufferInitDescriptor, DeviceExt};
 use pollster::FutureExt;
 
+use crate::matrix::Matrix;
+
 
 
 
@@ -40,5 +42,96 @@ impl MatrixMultiplier {
             queue,
             pipeline
         };
+    }
+
+    pub async fn run_once(&self, input_data: Vec<f32>, weights: Matrix) -> anyhow::Result<Vec<f32>> {
+        let input_buffer: wgpu::Buffer = self.device.create_buffer_init(&BufferInitDescriptor {
+            label: Some("input"),
+            contents: bytemuck::cast_slice(&input_data),
+            usage: wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::STORAGE,
+        });
+
+        let output_buffer = self.device.create_buffer(&wgpu::BufferDescriptor {
+            label: Some("output"),
+            size: input_buffer.size(),
+            usage: wgpu::BufferUsages::COPY_SRC | wgpu::BufferUsages::STORAGE,
+            mapped_at_creation: false,
+        });
+    
+        let temp_buffer = self.device.create_buffer(&wgpu::BufferDescriptor {
+            label: Some("temp"),
+            size: input_buffer.size(),
+            usage: wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::MAP_READ,
+            mapped_at_creation: false,
+        });
+
+
+
+
+        let bind_group = self.device.create_bind_group(&wgpu::BindGroupDescriptor {
+            label: None,
+            layout: &self.pipeline.get_bind_group_layout(0),
+            entries: &[
+                wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: input_buffer.as_entire_binding(),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 1,
+                    resource: output_buffer.as_entire_binding(),
+                },
+            ],
+        });
+
+        let mut encoder = self.device.create_command_encoder(&Default::default());
+
+        {
+            // We specified 64 threads per workgroup in the shader, so we need to compute how many
+            // workgroups we need to dispatch.
+            let num_dispatches = input_data.len().div_ceil(64) as u32;
+
+            let mut pass = encoder.begin_compute_pass(&Default::default());
+            pass.set_pipeline(&self.pipeline);
+            pass.set_bind_group(0, &bind_group, &[]);
+            pass.dispatch_workgroups(num_dispatches, 1, 1);
+        }
+
+        encoder.copy_buffer_to_buffer(&output_buffer, 0, &temp_buffer, 0, output_buffer.size());
+
+        self.queue.submit([encoder.finish()]);
+
+        let output = {
+            // The mapping process is async, so we'll need to create a channel to get
+            // the success flag for our mapping
+            let (tx, rx) = bounded(1);
+
+            // We send the success or failure of our mapping via a callback
+            temp_buffer.map_async(wgpu::MapMode::Read, .., move |result| {
+                tx.send(result).unwrap()
+            });
+
+            // The callback we submitted to map async will only get called after the
+            // device is polled or the queue submitted
+            self.device.poll(wgpu::PollType::wait_indefinitely())?;
+
+            // We check if the mapping was successful here
+            rx.recv_async().await??;
+
+            // We then get the bytes that were stored in the buffer
+            let output_data = temp_buffer.get_mapped_range(..);
+            
+            // Now we have the data on the CPU we can do what ever we want to with it
+            assert_eq!(&input_data, bytemuck::cast_slice(&output_data));
+
+            bytemuck::cast_slice(&output_data).to_vec()
+        };
+
+        // We need to unmap the buffer to be able to use it again
+        temp_buffer.unmap();
+
+        println!("Success!");
+
+        Ok(output)
+
     }
 }
---
+++ src/matrix.rs
@@ -0,0 +1,32 @@
+
+
+
+
+pub struct Matrix {
+    inner: Vec<Vec<f32>>,
+    width: usize,
+    height: usize
+}
+
+
+impl Matrix {
+    pub fn new(data: Vec<Vec<f32>>) -> Option<Self> {
+        let height = data.len();
+        let mut width: Option<usize> = None;
+
+        for row in data.iter() {
+            match width {
+                None => {
+                    width = Some(row.len());
+                },
+                Some(width_inner) => {
+                    if width_inner != row.len() {
+                        return None
+                    }
+                }
+            }
+        }
+
+        return Some(Self { inner: data, width: width?, height })
+    }
+}
