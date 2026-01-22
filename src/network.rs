5058fedc63fb1e68194168cf06b9c21f74eb1651
Zimbobub <89832179+Zimbobub@users.noreply.github.com>
2026-01-23 00:04:45 +1100

    remove descriptor code

--- src/network.rs
+++ src/network.rs
@@ -2,31 +2,7 @@ use crate::matrix::Matrix;
 
 
 
-pub struct NeuralNetworkDescriptor {
-    pub structure: Vec<usize>
-}
 
-impl NeuralNetworkDescriptor {
-    pub fn new(structure: Vec<usize>) -> Self {
-        return Self { structure }
-    }
-
-
-    pub fn matrix_sizes(&self) -> Vec<(usize, usize)> {
-        let mut matricies: Vec<(usize, usize)> = Vec::new();
-    
-        if self.structure.len() < 2 { return matricies }
-    
-        for (i, &layer) in self.structure.iter().enumerate() {
-            if i == 0 { continue; }
-    
-            matricies.push((self.structure[i-1], layer));
-        }
-    
-        return matricies;
-    }
-    
-}
 
 
 
