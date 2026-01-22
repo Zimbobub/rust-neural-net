0a16ffdf41d242d22de488ffc4827974284131c0
Zimbobub <89832179+Zimbobub@users.noreply.github.com>
2026-01-22 23:12:03 +1100

    get matrix sizes from descriptor

--- src/network.rs
+++ src/network.rs
@@ -3,9 +3,34 @@ use crate::matrix::Matrix;
 
 
 pub struct NeuralNetworkDescriptor {
-    structure: Vec<usize>
+    pub structure: Vec<usize>
 }
 
+impl NeuralNetworkDescriptor {
+    pub fn new(structure: Vec<usize>) -> Self {
+        return Self { structure }
+    }
+
+
+    pub fn matrix_sizes(&self) -> Vec<(usize, usize)> {
+        let mut matricies: Vec<(usize, usize)> = Vec::new();
+
+        if self.structure.len() < 2 { return matricies }
+
+        for (i, &layer) in self.structure.iter().enumerate() {
+            if i == 0 { continue; }
+
+            matricies.push((self.structure[i-1], layer));
+        }
+
+        return matricies;
+    }
+}
+
+
+pub trait NeuralNetwork {
+
+}
 
 pub struct MyNeuralNetwork {
     descriptor: NeuralNetworkDescriptor,
